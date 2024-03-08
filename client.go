fb9bf6744ed344e8db227601cd605a1fe965d19a
Newt <guoching130@gmail.com>
2024-03-08 18:42:49 +0800

    feat: add client.go

---
+++ client.go
@@ -0,0 +1,41 @@
+package mexcgo
+
+import (
+	"context"
+	"io"
+	"net/http"
+)
+
+const (
+    BaseEndpoint string = "https://api.mexc.com"
+)
+
+type Client struct {
+    apiKey          string
+    secretKey       string
+    baseEndpoint    string
+}
+
+func NewClient(apiKey, secretKey string) *Client {
+    return &Client {
+        apiKey: apiKey,
+        secretKey: secretKey,
+        baseEndpoint: BaseEndpoint,
+    }
+}
+
+func (c Client) GetApiKey() string { return c.apiKey }
+func (c *Client) SetApiKey(apiKey string) { c.apiKey = apiKey }
+
+func (c Client) GetSecretKey() string { return c.secretKey }
+func (c *Client) SetSecretKey(secretKey string) { c.secretKey = secretKey }
+
+func (c *Client) callApi(ctx context.Context, req *http.Request) ([]byte, error) {
+    res, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return nil, err
+    }
+    defer res.Body.Close()
+
+    return io.ReadAll(res.Body)
+}
